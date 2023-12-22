362205465f6ae5a820bf0dedbf48b84e2743131f
矢島英美里 <em202304@yajimaeimisatonoMacBook-Pro.local>
2023-12-22 10:58:27 +0900

    feat: 利用規約・このアプリについて・プライバシーポリシーの雛形

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root 'top#index'
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  get 'terms_of_use', to: 'top#terms_of_use'
+  get 'about', to: 'top#about'
+  get 'privacy_policy', to: 'top#privacy_policy'
 end
