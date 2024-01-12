bd0a85b249a9977ec05f94fa0a0a7d2dfc912cf3
矢島英美里 <em202304@yajimaeimisatonoMacBook-Pro.local>
2024-01-12 17:38:18 +0900

    feat: sorceryユーザー新規登録機能の追加

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
   get 'terms_of_use', to: 'top#terms_of_use'
   get 'about', to: 'top#about'
   get 'privacy_policy', to: 'top#privacy_policy'
+
+  resources :users, only: %i[new create]
 end
