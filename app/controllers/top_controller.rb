ebb46388b475412593af682862a0a14ff9eaf7e4
矢島英美里 <em202304@yajimaeimisatonoMacBook-Pro.local>
2024-01-30 02:11:17 +0900

    feat: 新規投稿に投稿者名を追加

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -2,8 +2,8 @@ class TopController < ApplicationController
   skip_before_action :require_login
 
   def index
-    @new_merchandise = Post.merchandise.order(created_at: :DESC).includes(:tags, :post_stamps).limit(10)
-    @new_showroom = Post.showroom.order(created_at: :DESC).includes(:tags, :post_stamps).limit(10)
+    @new_merchandise = Post.merchandise.order(created_at: :DESC).includes(:user, :profile, :tags, :post_stamps).limit(10)
+    @new_showroom = Post.showroom.order(created_at: :DESC).includes(:user, :profile, :tags, :post_stamps).limit(10)
   end
   def terms_of_use;end
   def about;end
