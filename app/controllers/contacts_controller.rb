32f839c046d6c0c544948e68ebf81c1f22498055
矢島英美里 <em202304@yajimaeimisatonoMacBook-Pro.local>
2024-01-31 17:49:36 +0900

    feat: 問い合わせフォームのビューおよびコントローラの作成

---
+++ app/controllers/contacts_controller.rb
@@ -0,0 +1,25 @@
+class ContactsController < ApplicationController
+  skip_before_action :require_login
+
+  def new
+    @contact = Contact.new
+  end
+
+  def create
+    @contact = Contact.new(contact_params)
+    @contact.login_user = 'name:' + current_user.name + ',id:' + current_user.id.to_s if logged_in?
+    if @contact.save
+      redirect_to done_contacts_path
+    else
+      render :new, status: :unprocessable_entity 
+    end
+  end
+
+  def done;end
+
+  private
+
+  def contact_params
+    params.require(:contact).permit(:name,:email,:email_confirmation,:subject,:message)
+  end
+end
--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,6 +1,7 @@
 class Contact < ApplicationRecord
   validates :name, presence: true, length: { maximum: 255 }
   validates :email, presence: true, confirmation: { message: "メールアドレスが一致しません" }, length: { maximum: 255 }
+  validates :email_confirmation, presence: true, length: { maximum: 255 }
   validates :subject, presence: true
   validates :message, presence: true, length: { maximum: 65_535 }
   validates :login_user, length: { maximum: 255 }
