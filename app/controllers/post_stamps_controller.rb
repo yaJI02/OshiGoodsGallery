8e44f5df0644eef0043559f77b4d42a776cc2427
矢島英美里 <em202304@yajimaeimisatonoMacBook-Pro.local>
2024-02-17 01:58:30 +0900

    fix: turbo送信するデータを修正

--- app/controllers/post_stamps_controller.rb
+++ app/controllers/post_stamps_controller.rb
@@ -1,27 +1,32 @@
 class PostStampsController < ApplicationController
   before_action :set_stamps, only: %i[registration destroy]
+  before_action :stamp_authorized, only: %i[registration]
 
   def registration
-    if @stamp.present?
-      redirect_to request.referer, flash: { danger: t('flash.post_stamp.danger', item: @stamp.stamp_i18n) }
-    elsif params[:stamp] == 'nice' && @post.user == current_user
-      redirect_to request.referer, flash: { danger: t('flash.post_stamp.mypost') }
-    else
-      @stamp = @post.post_stamps.create(user_id: current_user.id, stamp: PostStamp.stamps[params[:stamp]])
-      flash.now[:success] = t('flash.post_stamp.success', item: @stamp.stamp_i18n)
-      render turbo_stream: [
-        turbo_stream.replace("#{@stamp.stamp}-stamp-yet", partial: "reaction_stamps/#{@stamp.stamp}_done", locals: { post: @post }),
-        turbo_stream.replace('flash_messages', partial: 'shared/flash_message')
-      ]
-    end
+    @stamp = @post.post_stamps.create(user_id: current_user.id, stamp: PostStamp.stamps[params[:stamp]])
+    flash.now[:success] = t('flash.post_stamp.success', item: @stamp.stamp_i18n)
+    stamp_partial = @stamp.stamp == 'nice' ? 'reaction_stamps/nice_done' : 'reaction_stamps/done'
+    stamp_locals = { post: @post }
+    stamp_locals[:stamp] = @stamp.stamp if @stamp.stamp != 'nice'
+    stamp_locals[:icon] = @stamp.icon_name if @stamp.stamp != 'nice'
+
+    render turbo_stream: [
+      turbo_stream.replace("#{@stamp.stamp}-stamp-yet", partial: stamp_partial, locals: stamp_locals),
+      turbo_stream.replace('flash_messages', partial: 'shared/flash_message')
+    ]
   end
 
   def destroy
     @stamp.destroy
 
     flash.now[:success] = t('flash.post_stamp.destroy', item: @stamp.stamp_i18n)
+    stamp_partial = @stamp.stamp == 'nice' ? 'reaction_stamps/nice_yet' : 'reaction_stamps/yet'
+    stamp_locals = { post: @post }
+    stamp_locals[:stamp] = @stamp.stamp if @stamp.stamp != 'nice'
+    stamp_locals[:icon] = @stamp.icon_name if @stamp.stamp != 'nice'
+
     render turbo_stream: [
-      turbo_stream.replace("#{@stamp.stamp}-stamp-done", partial: "reaction_stamps/#{@stamp.stamp}_yet", locals: { post: @post }),
+      turbo_stream.replace("#{@stamp.stamp}-stamp-done", partial: stamp_partial, locals: stamp_locals),
       turbo_stream.replace('flash_messages', partial: 'shared/flash_message')
     ]
   end
@@ -31,5 +36,10 @@ class PostStampsController < ApplicationController
   def set_stamps
     @post = Post.find(params[:id])
     @stamp = PostStamp.find_by(post_id: @post, user_id: current_user.id, stamp: params[:stamp])
+  end
+
+  def stamp_authorized
+    redirect_to request.referer, flash: { danger: t('flash.post_stamp.mypost') } if params[:stamp] == 'nice' && @post.user == current_user
+    redirect_to request.referer, flash: { danger: t('flash.post_stamp.danger', item: @stamp.stamp_i18n) } if @stamp.present?
   end
 end
