caeb1293ad7898388fd9b6ab8e44ce8eccd338c6
矢島英美里 <em202304@yajimaeimisatonoMacBook-Pro.local>
2024-02-23 02:31:32 +0900

    feat: コメントモデルを追加

--- app/models/post.rb
+++ app/models/post.rb
@@ -7,6 +7,7 @@ class Post < ApplicationRecord
   has_many :tags, through: :post_tags, dependent: :destroy
   has_many :post_stamps, dependent: :destroy
   has_many :my_lists, dependent: :destroy
+  has_many :comments, dependent: :destroy
   has_one :profile # rubocop:disable Rails/HasManyOrHasOneDependent
 
   validates :title, presence: true, length: { maximum: 255 }
