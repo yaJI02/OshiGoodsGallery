a36358fadfc8845c5e6f3cef63107b06d956955c
矢島英美里 <em202304@yajimaeimisatonoMacBook-Pro.local>
2024-03-03 02:24:00 +0900

    fix: swiper.jsの読み込みの記述を修正

--- app/javascript/controllers/swiper.js
+++ app/javascript/controllers/swiper.js
@@ -1,46 +1,49 @@
 document.addEventListener('turbo:load', function () {
-  const swiperOptions = {
-    slidesPerView: 1,
-    breakpoints: {
-    768: {
-      slidesPerView: 2,
+  const swiper = document.querySelector('.swiper1');
+  if (swiper) {
+    const swiperOptions = {
+      slidesPerView: 1,
+      breakpoints: {
+      768: {
+        slidesPerView: 2,
+      },
+      992: {
+        slidesPerView: 3,
+      }
     },
-    992: {
-      slidesPerView: 3,
-    }
-  },
-    spaceBetween: 20,
-    centeredSlides: true,
+      spaceBetween: 20,
+      centeredSlides: true,
 
-    pagination: {
-      el: '.swiper-pagination',
-      clickable: true,
-    },
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+      },
 
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
 
-    scrollbar: {
-      el: '.swiper-scrollbar',
-    },
-  };
+      scrollbar: {
+        el: '.swiper-scrollbar',
+      },
+    };
 
-  const swipers = ['.swiper1', '.swiper2', '.swiper3'];
-  swipers.forEach(function (swiper_name) {
-    let slideshow = document.querySelector(swiper_name);
-    let slidesCount = slideshow.querySelectorAll('.swiper-slide').length;
-    let autoplayEnabled = slidesCount > 3 ? true : false;
-    let options = Object.assign({}, swiperOptions, {
-      slidesPerView: slidesCount <= 3 ? slidesCount : 3,
-      autoplay: autoplayEnabled
-        ? {
-            delay: 5000,
-            disableOnInteraction: false,
-          }
-        : false,
+    const swipers = ['.swiper1', '.swiper2', '.swiper3'];
+    swipers.forEach(function (swiper_name) {
+      let slideshow = document.querySelector(swiper_name);
+      let slidesCount = slideshow.querySelectorAll(".swiper-slide").length;
+      let autoplayEnabled = slidesCount > 3 ? true : false;
+      let options = Object.assign({}, swiperOptions, {
+        slidesPerView: slidesCount <= 3 ? slidesCount : 3,
+        autoplay: autoplayEnabled
+          ? {
+              delay: 5000,
+              disableOnInteraction: false,
+            }
+          : false,
+      });
+      new Swiper(swiper_name, options);
     });
-    new Swiper(swiper_name, options);
-  });
+  }
 });
