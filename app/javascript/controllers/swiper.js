74301c586dc716a4856d2f95082e16153c496969
矢島英美里 <em202304@yajimaeimisatonoMacBook-Pro.local>
2024-03-02 17:48:39 +0900

    feat: swiperについて投稿が３つ以上ある時のみautoplayを適用するように変更

--- app/javascript/controllers/swiper.js
+++ app/javascript/controllers/swiper.js
@@ -1,5 +1,5 @@
-document.addEventListener("turbo:load", function () {
-  const swiper = new Swiper(".swiper", {
+document.addEventListener('turbo:load', function () {
+  const swiperOptions = {
     slidesPerView: 1,
     breakpoints: {
     768: {
@@ -11,23 +11,36 @@ document.addEventListener("turbo:load", function () {
   },
     spaceBetween: 20,
     centeredSlides: true,
-    autoplay: {
-      delay: 5000,
-      disableOnInteraction: false,
-    },
 
     pagination: {
-      el: ".swiper-pagination",
+      el: '.swiper-pagination',
       clickable: true,
     },
 
     navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
     },
 
     scrollbar: {
-      el: ".swiper-scrollbar",
+      el: '.swiper-scrollbar',
     },
+  };
+
+  const swipers = ['.swiper1', '.swiper2', '.swiper3'];
+  swipers.forEach(function (swiper_name) {
+    let slideshow = document.querySelector(swiper_name);
+    let slidesCount = slideshow.querySelectorAll('.swiper-slide').length;
+    let autoplayEnabled = slidesCount > 3 ? true : false;
+    let options = Object.assign({}, swiperOptions, {
+      slidesPerView: slidesCount <= 3 ? slidesCount : 3,
+      autoplay: autoplayEnabled
+        ? {
+            delay: 5000,
+            disableOnInteraction: false,
+          }
+        : false,
+    });
+    new Swiper(swiper_name, options);
   });
 });
