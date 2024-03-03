683c9660be85c271817e46227b7c430f2ebf5b52
矢島英美里 <em202304@yajimaeimisatonoMacBook-Pro.local>
2024-03-04 06:30:25 +0900

    refactor: rubocopによるリファクタリング

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,12 +2,12 @@ module ApplicationHelper
   def default_meta_tags(page_title = '')
     {
       site: '推しグッズギャラリー',
-      title: page_title.empty? ? '' : "#{page_title}",
+      title: page_title.empty? ? '' : page_title.to_s,
       reverse: true,
       separator: '|',
       description: '推しのグッズを共有して交流するサイト♪',
-      canonical: request.original_url,   #優先するurlを指定する
-      noindex: ! Rails.env.production?,
+      canonical: request.original_url, # 優先するurlを指定する
+      noindex: !Rails.env.production?,
       icon: [
         { href: image_url('moon-icon-v.png') },
         { href: image_url('moon-icon-v.png'), rel: 'apple-touch-icon', sizes: '180x180', type: 'image/png' }
@@ -18,11 +18,11 @@ module ApplicationHelper
         type: 'website',
         url: request.original_url,
         image: image_url('sample.jpg'),
-        locale: 'ja_JP',
+        locale: 'ja_JP'
       },
       twitter: {
         card: 'summary_large_image',
-        image: image_url('sample.jpg'),
+        image: image_url('sample.jpg')
       }
     }
   end
