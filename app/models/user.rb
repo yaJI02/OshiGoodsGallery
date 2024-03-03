ca155c66b0a10d6703279f44f52e949c187796cd
矢島英美里 <em202304@yajimaeimisatonoMacBook-Pro.local>
2024-03-04 02:40:39 +0900

    feat: followモデルの関連付け

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,10 @@ class User < ApplicationRecord
   has_many :my_lists, dependent: :destroy
   has_many :comments, dependent: :destroy
   has_many :notifications, dependent: :destroy
+  has_many :follower, class_name: 'Follow', foreign_key: 'follower_id', dependent: :destroy
+  has_many :followed, class_name: 'Follow', foreign_key: 'followed_id', dependent: :destroy
+  has_many :following_user, through: :follower, source: :followed # 自分がフォローしている人
+  has_many :follower_user, through: :followed, source: :follower # 自分をフォローしている人
   has_one :profile, dependent: :destroy
 
   validates :password, length: { minimum: 5 }, if: -> { new_record? || changes[:crypted_password] }
