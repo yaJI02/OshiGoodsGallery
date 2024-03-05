1292af730f66779224a61885ca8ce9723c95fc1c
矢島英美里 <em202304@yajimaeimisatonoMacBook-Pro.local>
2024-03-05 20:50:20 +0900

    feat: googleログインボタンの追加

---
+++ app/controllers/oauths_controller.rb
@@ -0,0 +1,38 @@
+class OauthsController < ApplicationController
+  skip_before_action :require_login
+
+  def oauth
+    return redirect_to root_path, flash: { danger: t('flash.login.danger') } if logged_in?
+    login_at(auth_params[:provider])
+  end
+
+  def callback
+    provider = auth_params[:provider]
+    begin
+      if @user = login_from(provider)
+        if @user.profile.nil?
+          @profile = Profile.create(user: @user)
+          redirect_to edit_profile_path(@profile), flash: { success: t('flash.oauth_login.success', item: provider.titleize) }
+        else
+          redirect_to root_path, flash: { success: t('flash.oauth_login.success', item: provider.titleize) }
+        end
+      else
+        begin
+          @user = create_from(provider)
+          reset_session
+          auto_login(@user)
+          @profile = Profile.create(user: current_user)
+          redirect_to edit_profile_path(@profile), flash: { success: t('flash.oauth_login.success', item: provider.titleize) }
+        rescue
+          redirect_to login_path, flash: { danger: t('flash.oauth_login.danger', item: provider.titleize) }
+        end
+      end
+    end
+  end
+
+  private
+
+  def auth_params
+    params.permit(:code, :provider)
+  end
+end
