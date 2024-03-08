d8fca4d5664eb98807d91fe415f060a7785dfabb
矢島英美里 <em202304@yajimaeimisatonoMacBook-Pro.local>
2024-03-08 20:48:25 +0900

    fix: googleログインボタンのturboをfalseに変更

--- app/controllers/oauths_controller.rb
+++ app/controllers/oauths_controller.rb
@@ -3,29 +3,28 @@ class OauthsController < ApplicationController
 
   def oauth
     return redirect_to root_path, flash: { danger: t('flash.login.danger') } if logged_in?
+
     login_at(auth_params[:provider])
   end
 
   def callback
     provider = auth_params[:provider]
-    begin
-      if @user = login_from(provider)
-        if @user.profile.nil?
-          @profile = Profile.create(user: @user)
-          redirect_to edit_profile_path(@profile), flash: { success: t('flash.oauth_login.success', item: provider.titleize) }
-        else
-          redirect_to root_path, flash: { success: t('flash.oauth_login.success', item: provider.titleize) }
-        end
+    if @user = login_from(provider)
+      if @user.profile.nil?
+        @profile = Profile.create(user: @user)
+        redirect_to edit_profile_path(@profile), flash: { success: t('flash.oauth_login.success', item: provider.titleize) }
       else
-        begin
-          @user = create_from(provider)
-          reset_session
-          auto_login(@user)
-          @profile = Profile.create(user: current_user)
-          redirect_to edit_profile_path(@profile), flash: { success: t('flash.oauth_login.success', item: provider.titleize) }
-        rescue
-          redirect_to login_path, flash: { danger: t('flash.oauth_login.danger', item: provider.titleize) }
-        end
+        redirect_to root_path, flash: { success: t('flash.oauth_login.success', item: provider.titleize) }
+      end
+    else
+      begin
+        @user = create_from(provider)
+        reset_session
+        auto_login(@user)
+        @profile = Profile.create(user: current_user)
+        redirect_to edit_profile_path(@profile), flash: { success: t('flash.oauth_login.success', item: provider.titleize) }
+      rescue
+        redirect_to login_path, flash: { danger: t('flash.oauth_login.danger', item: provider.titleize) }
       end
     end
   end
